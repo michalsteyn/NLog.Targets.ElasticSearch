fa8d312500b2dcbe33e890023ff17c4a8792a1f1
Keith Williams <kwilliams@laneautomotive.com>
2016-02-23 11:11:22 -0500

    Updated some project meta data.

--- src/NLog.Targets.ElasticSearch/Properties/AssemblyInfo.cs
+++ src/NLog.Targets.ElasticSearch/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NLog.Targets.ElasticSearch")]
-[assembly: AssemblyDescription("NLog target for ElasticSearch")]
+[assembly: AssemblyTitle("NLog.Targets.ElasticSearch2")]
+[assembly: AssemblyDescription("NLog target for ElasticSearch 2.0")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ReactiveMarkets")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NLog.Targets.ElasticSearch")]
-[assembly: AssemblyCopyright("Copyright © ReactiveMarkets 2014")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
